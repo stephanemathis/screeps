6948e7a956ca1d0189c6c316d13f53db7a823f27
Stéphane Mathis <mathis.steph@gmail.com>
2016-12-13 20:58:40 +0100

    Improve miner repartition

--- common/role.miner.js
+++ common/role.miner.js
@@ -2,18 +2,45 @@ var roleMiner = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-
+        
         if(creep.carry.energy == creep.carryCapacity)
         {
             creep.drop(RESOURCE_ENERGY);
         }
-        else
+        else 
         {
-            // On va chercher de l'énergie
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE)
+            if(!creep.memory.targetSourceId)
             {
-                creep.moveTo(sources[0]);
+                var sources = creep.room.find(FIND_SOURCES);
+                for(var i = 0 ; i < sources.length ; i++)
+                {
+                    var pos = sources[0].pos;
+                    var result = creep.room.lookAtArea(pos.y - 1, pos.x - 12, pos.y + 1, pos.x + 1, true);
+                    
+                    var numberOfMiners = result.filter((p) => { p.type == "creep" && p.creep.memory.role == "miner" });
+                    
+                    if(numberOfMiners == 0)
+                    {
+                        var isSpotReserved = false;
+                        for(var creepName in Game.creeps)
+                        {
+                            if(Game.creeps[creepName].memory.targetSourceId == sources[i].id)
+                                isSpotReserved = true;
+                        }
+                        if(!isSpotReserved)
+                            creep.memory.targetSourceId = sources[i].id;
+                    }
+                }
+            }
+            
+            var source = Game.getObjectById(creep.memory.targetSourceId);
+            
+            if(!source)
+                creep.memory.targetSourceId = null;
+            
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) 
+            {
+                creep.moveTo(source);
             }
         }
     }
