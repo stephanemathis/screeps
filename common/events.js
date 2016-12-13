4eefb41e4201ce44de523d40a8e59a6b91b791cd
Stéphane Mathis <mathis.steph@gmail.com>
2016-12-13 21:07:06 +0100

    increase citizen

--- common/events.js
+++ common/events.js
@@ -5,36 +5,36 @@ module.exports = {
     tick()
     {
         var controllerLevel = this.GetControllerLevel();
-
+        
         if(controllerLevel != Memory.ControllerLevel)
             this.OnControllerLevelChanged(controllerLevel);
     },
 
-    init()
+    init() 
     {
         if(Memory.ControllerLevel === undefined)
         {
             Memory.ControllerLevel = this.GetControllerLevel();
         }
     },
-
+    
     OnControllerLevelChanged(newLevel)
     {
         Memory.ControllerLevel = newLevel;
-
+    
         console.log("New controller level : " + newLevel);
-
+    
         if(newLevel == 2)
         {
-            spawner.addToSpawnQueue(["upgrader", "citizen", "citizen", "citizen"]);
+            spawner.addToSpawnQueue(["upgrader", "citizen", "citizen", "citizen", "citizen", "citizen", "citizen"]);
         }
     },
-
+    
     GetControllerLevel()
     {
         return this.GetRoom().controller.level;
     },
-
+    
     GetRoom()
     {
         for(var name in Game.rooms)
