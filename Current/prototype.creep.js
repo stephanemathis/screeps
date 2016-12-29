5066d88b75bcbf6ec94e88104f4c6f0c006c87e1
Stéphane Mathis <mathis.steph@gmail.com>
2016-12-29 18:34:35 +0100

    Reverse energy search order

--- Current/prototype.creep.js
+++ Current/prototype.creep.js
@@ -1,23 +1,9 @@
 module.exports = function() {
-    /**/
+    
     Creep.prototype.findAndPickEnergy = function ()
     {
         var creep = this;
         var needMoreEnergy = true;
-
-        if (needMoreEnergy) {
-            var sources = creep.room.find(FIND_DROPPED_ENERGY);
-            if (sources.length > 0) {
-                var pickResult = creep.pickup(sources[0]);
-                if (pickResult == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(sources[0]);
-                    needMoreEnergy = false;
-                }
-
-                if (pickResult == OK)
-                    needMoreEnergy = false;
-            }
-        }
 
         if (needMoreEnergy) {
             var containersWithEnergy = creep.room.find(FIND_STRUCTURES, {
@@ -32,6 +18,21 @@ module.exports = function() {
                 }
 
                 if (withdrawResult == OK)
+                    needMoreEnergy = false;
+            }
+        }
+
+        if (needMoreEnergy) {
+            var sources = creep.room.find(FIND_DROPPED_ENERGY);
+
+            if (sources.length > 0) {
+                var pickResult = creep.pickup(sources[0]);
+                if (pickResult == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(sources[0]);
+                    needMoreEnergy = false;
+                }
+
+                if (pickResult == OK)
                     needMoreEnergy = false;
             }
         }
